90bcd91286b927fa887e209c021400d8fa2096bf
Arthur <artu.donascimento@gmail.com>
2025-11-23 03:03:18 -0300

    create not authorized page

--- app/not-authorized/page.tsx
+++ app/not-authorized/page.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from '@/components/ui/empty'
+import { ArrowUpRightIcon, Folder, FolderX } from 'lucide-react'
+import Link from 'next/link'
 
 const page = () => {
   return (
-    <div>
-      não autorizado
-    </div>
-  )
+    <main className="dark:bg-zinc-900 w-dvw h-dvh flex items-center justify-center">
+      <Empty>
+        <EmptyHeader>
+          <EmptyMedia variant="icon">
+            <FolderX />
+          </EmptyMedia>
+          <EmptyTitle>Não autorizado</EmptyTitle>
+          <EmptyDescription>
+            Você não tem permissão para acessar esta página.
+          </EmptyDescription>
+        </EmptyHeader>
+        <EmptyContent>
+          <div className="flex gap-2">
+            <Button>
+              <Link href={"/registry/record/create"}>Novo Registro</Link>
+            </Button>
+            <Button variant="outline">
+              <Link href={"/registry/history"}>Ver Registros</Link>
+            </Button>
+          </div>
+        </EmptyContent>
+        <Button
+          variant="link"
+          asChild
+          className="text-muted-foreground"
+          size="sm"
+        ></Button>
+      </Empty>
+    </main>
+  );
 }
 
 export default page
